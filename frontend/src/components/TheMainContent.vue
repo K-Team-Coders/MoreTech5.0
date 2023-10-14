7396b669b9e0714dd3d3109f0118ca2f45a01f34
Shisha <shishkarev16@gmail.com>
2023-10-14 14:29:27 +0300

    Update TheMainContent.vue

--- frontend/src/components/TheMainContent.vue
+++ frontend/src/components/TheMainContent.vue
@@ -1,23 +1,43 @@
 <template>
   <div class="ml-64 pt-[70px] font-Montserrat">
-    <div class="px-5 py-4 h-full bg-white" v-show="$attrs.activeindex === 0">
-      <div class="mb-4">
-        <Map :postamat_list="allpostamats.data"> </Map>
-      </div>
-      <div>
-        <RightDropSidebar />
-      </div>
-      <div
-        class="px-5 py-3 shadow-innerMax h-full"
-        v-show="$attrs.activeindex === 1"
-      >
-        <div class="h-full py-12">
-          <div class="flex flex-row justify-center gap-2"></div>
+    <transition
+    enter-active-class="transition ease-out duration-300"
+    enter-from-class="transform opacity-0 scale-95"
+    enter-to-class="transform opacity-100 scale-100"
+    leave-active-class="transition ease-in duration-75"
+    leave-from-class="transform opacity-100 scale-100"
+    leave-to-class="transform opacity-0 scale-95"
+    >
+      <div class="px-5 py-4 h-full bg-white " v-show="$attrs.activeindex === 0">
+        <div class="mb-4">
+          <Map :postamat_list="allpostamats.data"></Map>
+        </div>
+        <div>
+          <RightDropSidebar />
         </div>
       </div>
-    </div>
+    </transition>
+
+    <transition
+    enter-active-class="transition ease-out duration-300"
+    enter-from-class="transform opacity-0 scale-95"
+    enter-to-class="transform opacity-100 scale-100"
+    leave-active-class="transition ease-in duration-75"
+    leave-from-class="transform opacity-100 scale-100"
+    leave-to-class="transform opacity-0 scale-95"
+    >
+      <div
+        class="px-5 py-3 shadow-innerMax h-full"
+        v-if="$attrs.activeindex === 1"
+      >
+        <div class="h-full py-12">
+          <div class="flex flex-row justify-center gap-2">ssss</div>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
+
 <script>
 import Map from "@/components/Map.vue";
 import axios from "axios";
@@ -27,9 +47,6 @@ export default {
     Map,
     RightDropSidebar,
   },
-
-  methods: {},
-  computed: {},
   props: {
     allpostamats: Object,
   },
