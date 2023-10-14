87e4c9910aa45cad22a98f995eeeaca4af2439c6
qTemiO <71713499+qTemiO@users.noreply.github.com>
2023-10-14 11:20:28 +0300

    added based map data

--- fastApi/main.py
+++ fastApi/main.py
@@ -1,14 +1,56 @@
 import os
 import time
+import json
+import random
 from pathlib import Path
 
 import psycopg2
 from loguru import logger
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
+timings = {
+    "Кредит наличными": 7.6,
+    "Экспресс-кредит": 4.5,
+    "Рефинансирование": 8.9,
+    "Кредит под залог недвижимости": 10.9,
+    "Дебетовые карты": 7.3,
+    "Кредитные карты": 4.5,
+    "Пенсионные карты": 8.9,
+    "Карты жителя": 5.7,
+    "Социальные карты":12.3,
+    "Ипотека на готовый дом": 10.1,
+    "Ипотека на строящийся дом": 13.8,
+    "Кредит под залог имеющейся недвижимости": 7.8,
+    "Ипотека на машино-места и кладовки": 9.8,
+    "Ипотека с материнским капиталом": 7.8,
+    "Вклады": 9.8,
+    "Операции по счету": 8.2,
+    "Операции по инвестициям": 6.2,
+    "Обмен валют": 8.1,
+    "Оплата ЖКХ": 9.1,
+    "Расчетный счет": 5.6,
+    "Регистрация бизнеса": 9.5,
+    "Кредиты": 5.6,
+    "Бизнес-карты": 8.7,
+    "Эквайринг": 14.1,
+    "Депозиты": 12.1,
+    "ВЭД": 7.8,
+    "Гарантии и аккредитивы": 12.1,
+    "Сервисы для бизнеса": 9.2
+}
+
 PRODUCTION = False
+
+offices = 0
+with open('offices.txt', 'r', encoding='utf-8') as f:
+    offices = json.load(f) 
+
+atms = 0
+with open('atms.txt', 'r', encoding='utf-8') as f:
+    atms = json.load(f) 
 
 DBenv = Path().cwd().parent.joinpath("DB.env")
 load_dotenv(DBenv, override=True)
@@ -73,4 +115,49 @@ app.add_middleware(
 def root():
     return {"message": "Hello World"}
 
+# def countStats():
+    # cur.execute("SELECT * FROM ")
 
+@app.get("/videocam")
+def videocam(item: UploadFile):
+    pass
+
+def addQueue():
+    pass
+
+def getAtmsCoords():
+    results = []
+    for index, atm in enumerate(atms["atms"]):
+        result = {}
+        result["id"] = index 
+        result["address"] = atm["address"]   
+        result["longitude"] = atm["longitude"]
+        result["latitude"] = atm["latitude"]
+
+        results.append(result)
+    return results
+
+def getOfficesCoords():
+    results = []
+        
+    for index, office in enumerate(offices):
+        result = {}
+        result["id"] = index
+        result["name"] = office["salePointName"]
+        result["address"] = office["address"]   
+        result["openHours"] = office["openHours"]
+        result["longitude"] = office["longitude"]
+        result["latitude"] = office["latitude"]
+        result["services"] = random.sample(list(timings.keys()), 10)
+        results.append(result)
+    return results
+
+@app.get("/getAllBanks")
+def webAllBanks():
+    offices = getOfficesCoords()
+    atms = getAtmsCoords()
+    return JSONResponse(content={"offices": offices, "atms": atms}, status_code=200)
+
+@app.get("/videocam")
+def videocam(item: UploadFile):
+    pass
